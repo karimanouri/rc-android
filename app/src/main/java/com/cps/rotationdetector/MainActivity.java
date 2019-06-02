c5dd1c28ce959e3e536e8240bd42cb3f3479f400
iman <ikn77@yahoo.com>
2019-06-02 12:42:12 +0430

    add speed change make packet

--- app/src/main/java/com/cps/rotationdetector/MainActivity.java
+++ app/src/main/java/com/cps/rotationdetector/MainActivity.java
@@ -3,33 +3,55 @@ package com.cps.rotationdetector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
 public class MainActivity extends AppCompatActivity {
 
     private RotationAngleDetector.RotationAngleListener rotationAngleListener;
 
-    private TextView txtY, txtZ;
+    private TextView txtY, txtZ, txtSpeed;
     private EditText edtIpAddress;
+
+    private DatagramSocket socket;
+
+    private int speed = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtIpAddress = findViewById(R.id.edt_ip_address);
         txtY = findViewById(R.id.txt_y);
         txtZ = findViewById(R.id.txt_z);
-        edtIpAddress = findViewById(R.id.edt_ip_address);
+        txtSpeed = findViewById(R.id.txt_speed);
+        Button btnPlusSpeed = findViewById(R.id.btn_plus);
+        btnPlusSpeed.setOnClickListener(btnSpeedListener);
+        Button btnMinusSpeed = findViewById(R.id.btn_minus);
+        btnMinusSpeed.setOnClickListener(btnSpeedListener);
+        txtSpeed.setText(String.valueOf(speed));
 
         Sensey.getInstance().init(this, Sensey.SAMPLING_PERIOD_UI);
+
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
 
         rotationAngleListener = new RotationAngleDetector.RotationAngleListener() {
             @Override
             public void onRotation(float angleInAxisX, float angleInAxisY, float angleInAxisZ) {
                 txtY.setText(String.valueOf(angleInAxisY));
                 txtZ.setText(String.valueOf(angleInAxisZ));
-                UdpSend udpSend = new UdpSend(MainActivity.this, edtIpAddress.getText().toString());
-                udpSend.execute(angleInAxisY, angleInAxisZ);
+                UdpSend udpSend = new UdpSend(socket, edtIpAddress.getText().toString());
+                udpSend.execute(convertToAngle(angleInAxisY), speed);
             }
         };
 
@@ -41,5 +63,32 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Sensey.getInstance().stopRotationAngleDetection(rotationAngleListener);
         Sensey.getInstance().stop();
+        socket.close();
     }
+
+    private int convertToAngle(float y) {
+        int angle = 90 - (int) y;
+        if(angle > 180)
+            angle = 180;
+        else if(angle < 0)
+            angle = 0;
+        return angle / 3;
+    }
+
+    private View.OnClickListener btnSpeedListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btn_plus:
+                    speed = (speed < 4) ? speed + 1 : speed;
+                    Log.v("SpeedListener", String.valueOf(speed));
+                    break;
+                case R.id.btn_minus:
+                    speed = (speed > 1) ? speed - 1 : speed;
+                    Log.v("SpeedListener", String.valueOf(speed));
+                    break;
+            }
+            txtSpeed.setText(String.valueOf(speed));
+        }
+    };
 }
--- app/src/main/java/com/cps/rotationdetector/UdpSend.java
+++ app/src/main/java/com/cps/rotationdetector/UdpSend.java
@@ -1,39 +1,36 @@
 package com.cps.rotationdetector;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-class UdpSend extends AsyncTask<Float, Void, Boolean> {
+class UdpSend extends AsyncTask<Integer, Void, Boolean> {
 
     private String serverAddress;
-    private Context activity;
+    private DatagramSocket socket;
 
-    UdpSend(Context activity, String serverAddress) {
-        this.activity = activity;
+    public enum Mode{
+        STOP, MOVE, REVERSE_MOVE, GO_STRAIGHT, GO_BACK, TURN_RIGHT, TURN_LEFT
+    }
+
+    UdpSend(DatagramSocket socket, String serverAddress) {
+        this.socket = socket;
         this.serverAddress = serverAddress;
     }
 
     @Override
-    protected Boolean doInBackground(Float... params) {
-        String strMessage;
+    protected Boolean doInBackground(Integer... params) {
         final int server_port = 4210;
         byte[] message;
-        message = this.makePacket(params[0], params[1]);
+        message = this.makePacket(params[0], params[1], Mode.MOVE);
         try {
-            DatagramSocket s = new DatagramSocket();
             InetAddress address = InetAddress.getByName(serverAddress);
             DatagramPacket p = new DatagramPacket(message, message.length, address, server_port);
-            s.send(p);
+            socket.send(p);
         } catch (IOException e) {
             Log.e("UdpSend", e.getMessage());
             return false;
@@ -44,30 +41,17 @@ class UdpSend extends AsyncTask<Float, Void, Boolean> {
 
     @Override
     protected void onPostExecute(Boolean success) {
-//        if(!success)
-//            Toast.makeText(activity, "Please enter the ip address...", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     protected void onPreExecute() {
     }
 
-    private byte[] makePacket(float y, float z) {
-        int _y;
-        _y = 90-(int)y;
-        if(_y>180)_y=180;
-        if(_y<0)_y=0;
-        _y /= 3;
-        return make_packet(2,_y,Mode.MOVE);
-    }
-    //speed 1-4 angle 0-180
-    private byte[] make_packet(int speed,int angle,Mode mode){
+    private byte[] makePacket(int angle, int speed, Mode mode) {
         byte[] return_value = new byte[2];
-        return_value[0] = (byte)((int)((speed-1)<<6) + (int)((angle/3)&0x3F));
-        return_value[1] = (byte)(mode.ordinal()<<5);
+        return_value[0] = (byte)(((speed - 1) << 6) + ((angle / 3) & 0x3F));
+        return_value[1] = (byte)(mode.ordinal() << 5);
         return return_value;
-    }
-    public static enum Mode{
-        STOP,MOVE,REVERSE_MOVE,GO_STRAIGHT,GO_BACK,TURN_RIGHT,TURN_LEFT
     }
 }
