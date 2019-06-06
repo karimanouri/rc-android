27a7de3b75565e8fa93b8e3196541566cd24360f
iman <ikn77@yahoo.com>
2019-06-06 14:03:35 +0430

    replace listener object with implementation

--- app/src/main/java/com/cps/rotationdetector/MainActivity.java
+++ app/src/main/java/com/cps/rotationdetector/MainActivity.java
@@ -18,9 +18,8 @@ import android.widget.TextView;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, RotationAngleDetector.RotationAngleListener {
 
-    private RotationAngleDetector.RotationAngleListener rotationAngleListener;
 
     private TextView txtAngle, txtZ, txtSpeed;
     private LinearLayout linearLayoutAngle, linearLayoutZ;
@@ -38,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         initUiComponent();
-        initSocket();
         initRotationSensor();
         initStartSettingsIntent();
+        initSocket();
     }
 
     @Override
@@ -65,9 +64,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Sensey.getInstance().stopRotationAngleDetection(rotationAngleListener);
+        Sensey.getInstance().stopRotationAngleDetection(this);
         Sensey.getInstance().stop();
         socket.close();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_plus:
+                speed = (speed < 4) ? speed + 1 : speed;
+                Log.v("SpeedListener", String.valueOf(speed));
+                break;
+            case R.id.btn_minus:
+                speed = (speed > 1) ? speed - 1 : speed;
+                Log.v("SpeedListener", String.valueOf(speed));
+                break;
+        }
+        txtSpeed.setText(String.valueOf(speed));
+    }
+
+    @Override
+    public void onRotation(float angleInAxisX, float angleInAxisY, float angleInAxisZ) {
+        txtAngle.setText(String.valueOf(convertToAngle(angleInAxisY)));
+        txtZ.setText(String.valueOf(angleInAxisZ));
+        new UdpSend(socket, preferences.getString(getString(R.string.preference_key_ip), getString(R.string.default_ip))).execute(convertToAngle(angleInAxisY), speed);
     }
 
     private int convertToAngle(float y) {
@@ -79,23 +100,6 @@ public class MainActivity extends AppCompatActivity {
         return angle;
     }
 
-    private View.OnClickListener btnSpeedListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.btn_plus:
-                    speed = (speed < 4) ? speed + 1 : speed;
-                    Log.v("SpeedListener", String.valueOf(speed));
-                    break;
-                case R.id.btn_minus:
-                    speed = (speed > 1) ? speed - 1 : speed;
-                    Log.v("SpeedListener", String.valueOf(speed));
-                    break;
-            }
-            txtSpeed.setText(String.valueOf(speed));
-        }
-    };
-
     private void initUiComponent() {
         linearLayoutAngle = findViewById(R.id.line_angle);
         linearLayoutZ = findViewById(R.id.line_z);
@@ -104,9 +108,9 @@ public class MainActivity extends AppCompatActivity {
         txtZ = findViewById(R.id.txt_z);
         txtSpeed = findViewById(R.id.txt_speed);
         Button btnPlusSpeed = findViewById(R.id.btn_plus);
-        btnPlusSpeed.setOnClickListener(btnSpeedListener);
+        btnPlusSpeed.setOnClickListener(this);
         Button btnMinusSpeed = findViewById(R.id.btn_minus);
-        btnMinusSpeed.setOnClickListener(btnSpeedListener);
+        btnMinusSpeed.setOnClickListener(this);
         txtSpeed.setText(String.valueOf(speed));
     }
 
@@ -126,21 +130,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void initRotationSensor() {
         Sensey.getInstance().init(this, Sensey.SAMPLING_PERIOD_UI);
-        rotationAngleListener = new RotationAngleDetector.RotationAngleListener() {
-            @Override
-            public void onRotation(float angleInAxisX, float angleInAxisY, float angleInAxisZ) {
-                txtAngle.setText(String.valueOf(convertToAngle(angleInAxisY)));
-                txtZ.setText(String.valueOf(angleInAxisZ));
-                UdpSend udpSend = new UdpSend(socket, preferences.getString(getString(R.string.preference_key_ip), getString(R.string.default_ip)));
-                udpSend.execute(convertToAngle(angleInAxisY), speed);
-            }
-        };
-        Sensey.getInstance().startRotationAngleDetection(rotationAngleListener);
+        Sensey.getInstance().startRotationAngleDetection(this);
     }
 
     private void debugMode(boolean isDebug) {
         linearLayoutAngle.setVisibility(isDebug ? View.VISIBLE : View.GONE);
         linearLayoutZ.setVisibility(isDebug ? View.VISIBLE : View.GONE);
-        Log.v("MainActivity", String.valueOf(isDebug));
     }
 }
